cadc914781422d218ed754c84da94cf754c2edee
DouglasSouza05 <douglasbsouza01@gmail.com>
2023-09-14 16:46:34 -0300

    Adicionando funcionalidade e criando Teste Unitários

--- Desafio-Teste-Unitário-C214-Lab/DesafioTesteUnitario/src/imc.js
+++ Desafio-Teste-Unitário-C214-Lab/DesafioTesteUnitario/src/imc.js
@@ -1,10 +1,61 @@
 function calcularIMC(peso, altura) {
-  if (altura === 0) {
-    return "NaN"; // Retorna "NaN" se a altura for igual a zero
+  if (altura <= 0 || peso <= 0) {
+    return "NaN"; // Retorna "NaN" se a Altura ou Peso for igual a zero
+  }
+
+  if (altura >= 280 || peso >= 400) {
+    return "NaN"; // Retorna "NaN" se a Altura ou Peso for igual a zero
   }
 
   const alturaMetros = altura / 100; // Converte altura de centímetros para metros
   return (peso / (alturaMetros * alturaMetros)).toFixed(2); // Calcula o IMC com duas casas decimais
 }
 
-module.exports = { calcularIMC }; // Exporta a função calcularIMC
+function healthCheck(imc) {
+  if (imc <= 18.5) {
+    return "Você está abaixo do Peso ideal. Considere procurar ajuda médica!";
+  } else if (imc <= 24.9) {
+    return "Você está no Peso ideal. Continue mantendo-se em forma!";
+  } else if (imc <= 29.9) {
+    return "Você está levemente acima do Peso ideal. Considere fazer exercícios físicos!";
+  } else if (imc <= 34.9) {
+    return "Você está com Obesidade grau 1. Importante tomar medidas para melhorar a saúde!";
+  } else if (imc <= 39.9) {
+    return "Você está com Obesidade grau 2. Favor Consultar um médico!";
+  } else {
+    return "Você está com Obesidade grau 3. Busque assistência médica imediatamente!.";
+  }
+}
+
+function calculateWeight(altura, sexo) {
+  if (altura <= 0) {
+    return "NaN";
+  }
+
+  if (altura >= 280) {
+    return "NaN";
+  }
+
+  // Constantes para as fórmulas de cálculo de peso ideal baseado no Sexo
+  const fatorMasculino = 4.8;
+  const fatorFeminino = 4.7;
+
+  //   const alturaMetros = altura / 100; // Converte altura de centímetros para metros
+
+  let pesoIdeal;
+
+  // Calcule o peso ideal com base no sexo
+  if (sexo === "Masculino") {
+    pesoIdeal = altura - 100 - (altura - 150) / fatorMasculino;
+  } else if (sexo === "Feminino") {
+    pesoIdeal = altura - 100 - (altura - 150) / fatorFeminino;
+  } else {
+    return "Sexo não especificado";
+  }
+
+  //   console.log(pesoIdeal);
+
+  return pesoIdeal.toFixed(2); // Arredonde o resultado para duas casas decimais
+}
+
+module.exports = { calcularIMC, healthCheck, calculateWeight }; // Exporta a função calcularIMC
--- Desafio-Teste-Unitário-C214-Lab/DesafioTesteUnitario/test/imc.test.js
+++ Desafio-Teste-Unitário-C214-Lab/DesafioTesteUnitario/test/imc.test.js
@@ -1,23 +1,64 @@
 const { expect } = require("chai");
-const { calcularIMC } = require("../src/imc");
+const { calcularIMC, healthCheck, calculateWeight } = require("../src/imc");
 
 describe("Calculadora de IMC", () => {
-  it("Calculadora deve rodar tranquilo", () => {
+  it("Fazendo calculo do IMC!", () => {
     const peso = 80; // 80 kg
     const altura = 180; // 180 cm
 
-    const IMEsperado = (peso / Math.pow(altura / 100, 2)).toFixed(2);
-    const IMCCalculado = calcularIMC(peso, altura);
+    const expectedIMC = (peso / Math.pow(altura / 100, 2)).toFixed(2);
+    const calculatedIMC = calcularIMC(peso, altura);
 
-    expect(IMCCalculado).to.equal(IMEsperado);
+    expect(calculatedIMC).to.equal(expectedIMC);
   });
 
-  it("altura = 0, entrada inadequada", () => {
-    const peso = 70; // 70 kg
-    const altura = 0; // 0 cm
+  it("Altura ou Peso impossíveis. Entradas inadequadas!", () => {
+    let peso = 400; // 400 kg
+    let altura = 0;
 
-    const IMCCalculado = calcularIMC(peso, altura);
+    let IMC = calcularIMC(peso, altura);
 
-    expect(IMCCalculado).to.equal("NaN");
+    expect(IMC).to.equal("NaN");
+
+    peso = 0;
+    altura = 280; // 280 cm
+
+    IMC = calcularIMC(peso, altura);
+
+    expect(IMC).to.equal("NaN");
+  });
+
+  it("Consultando tabela de ajuda para Peso Normal!", () => {
+    const peso = 80; // 80 kg
+    const altura = 180; // 180 cm
+    const calculatedIMC = calcularIMC(peso, altura);
+
+    const description =
+      "Você está no Peso ideal. Continue mantendo-se em forma!";
+
+    expect(description).to.equal(healthCheck(calculatedIMC));
+  });
+
+  it("Consultando tabela de ajuda para Obesidade Grau 2!", () => {
+    const peso = 105;
+    const altura = 170;
+    const calculatedIMC = calcularIMC(peso, altura);
+
+    const description =
+      "Você está com Obesidade grau 2. Favor Consultar um médico!";
+
+    expect(description).to.equal(healthCheck(calculatedIMC));
+  });
+
+  it("Consultando Peso Ideal baseado no Sexo.", () => {
+    const altura = 170;
+    let sexo = "Masculino";
+
+    const pesoIdealM = parseFloat(calculateWeight(altura, sexo));
+    expect(65.83).to.equal(pesoIdealM);
+
+    sexo = "Feminino";
+    const pesoIdealF = parseFloat(calculateWeight(altura, sexo));
+    expect(65.74).to.equal(pesoIdealF);
   });
 });
